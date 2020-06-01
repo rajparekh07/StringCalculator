e1137e0db9af1869a7b5b9e077e708b5722d8fb4
Raj <raj.parekh07@gmail.com>
2020-06-02 02:09:13 +0530

    Simple string calculator for adding up-to 2 numbers.

--- src/codes/rajp/StringCalculator.java
+++ src/codes/rajp/StringCalculator.java
@@ -1,16 +1,84 @@
 package codes.rajp;
 
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * String Calculator
+ * This class implements a method which adds zero, one, or two numbers.
+ *
+ * @author  Raj Parekh
+ * @version 1
+ * @since   01-06-2020
+ *
+ */
 public class StringCalculator {
 
+
+    /**
+     *  An integer stating maximum numbers to be added.
+     */
+    private static final int MAX_NUMBERS_TO_BE_ADDED = 2;
+
+
+    /**
+     *  A Regular Expression for matching out the digits delimited with ","
+     */
+    public static final String DELIMITER = "(((?!,).)\\w?)+";
+
+    /**
+     * An empty constructor
+     */
     StringCalculator() {}
 
-    int returnTwo() {
 
-        return 2;
+    /**
+     * A method for simple addition method taking numbers as string input and returning the sum.
+     * @param numbers as a string separated by "," as a delimiter
+     * @return sum of numbers passed as the argument
+     */
+    public int add(String numbers) {
+
+        ArrayList<Integer> parsedNumbers = this.parseNumbers(numbers);
+
+        return parsedNumbers.stream().mapToInt(Integer::intValue).sum();
     }
 
+
+    /**
+     * parsing the String numbers to java.util.ArrayList list
+     * @param numbers as a string separated by "," as a delimiter
+     * @return ArrayList with all parsed numbers
+     * @throws Exception when illegal string character is found instead of number
+     * or when more than two
+     */
+    private ArrayList<Integer> parseNumbers(String numbers) {
+
+        int numbersCount = 1;
+        ArrayList<Integer> parsedNumbers = new ArrayList<>();
+
+        Pattern inputNumbersPattern = Pattern.compile(DELIMITER);
+        Matcher numbersMatcher = inputNumbersPattern.matcher(numbers);
+
+        while (numbersMatcher.find() && numbersCount <= MAX_NUMBERS_TO_BE_ADDED) {
+            String extractedNumber = numbersMatcher.group();
+            Integer parsedNumber = new Integer(extractedNumber);
+
+            parsedNumbers.add(parsedNumber);
+
+            numbersCount++;
+        }
+
+        return parsedNumbers;
+    }
+
+    /**
+     * A main driver method for String Calculator.
+     * @param args
+     */
     public static void main(String[] args) {
         StringCalculator st = new StringCalculator();
-        System.out.println(st.returnTwo());
+        System.out.println(st.add("1,2"));
     }
 }
--- tests/codes/rajp/StringCalculatorTest.java
+++ tests/codes/rajp/StringCalculatorTest.java
@@ -8,12 +8,41 @@ import static org.junit.Assert.*;
 public class StringCalculatorTest {
 
     @Test
-    public void testTwo() throws Exception {
+    public void testAddNumberOfArguments() throws Exception {
         StringCalculator st = new StringCalculator();
 
-        Assert.assertEquals(st.returnTwo(), 2);
-
+        Assert.assertEquals(3, st.add("1,2,3"));
+        Assert.assertEquals(11233, st.add("1,11232"));
+        Assert.assertEquals(22, st.add("22"));
+        Assert.assertEquals(2, st.add("2"));
     }
 
+    @Test(expected=RuntimeException.class)
+    public void testIllegalDigits() throws Exception {
+        StringCalculator st = new StringCalculator();
+
+        st.add("1,2a");
+    }
+
+    @Test(expected=RuntimeException.class)
+    public void testDifferentDelimiter() throws Exception {
+        StringCalculator st = new StringCalculator();
+
+        st.add("1:2");
+    }
+
+    @Test
+    public void testDifferentNumbers() throws Exception {
+        StringCalculator st = new StringCalculator();
+
+        Assert.assertEquals(-1, st.add("1,-2"));
+        Assert.assertEquals(1, st.add("-1,2"));
+        Assert.assertEquals(-2, st.add("-2"));
+        Assert.assertEquals(2, st.add("2,000000"));
+        Assert.assertEquals(2, st.add("00002,000000"));
+        Assert.assertEquals(2, st.add("2,+0"));
+        Assert.assertEquals(2, st.add("+2,+0"));
+
+    }
 
 }
